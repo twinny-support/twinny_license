00cef0c8e961ed87e8a65869670b20f3cac6ddf8
Twinny Support <twinsxstore@gmail.com>
2022-02-28 20:44:46 +0700

    Update license.js

--- license.js
+++ license.js
@@ -1,1 +1,1 @@
-var customLSN = ['localhost','twinnyblogger.github.io','twinnybloggers.blogspot.com]; var redirectURL = 'https://twinnyblogger.blogspot.com';
+var customLSN = ['localhost','twinnyblogger.github.io','twinnybloggers.blogspot.com']; var redirectURL = 'https://twinnyblogger.blogspot.com';
