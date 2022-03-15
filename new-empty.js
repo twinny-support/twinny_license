97495cdc1419df7375500700dfae997252c97b51
Twinny Support <21377149+twinnybloggers@users.noreply.github.com>
2022-03-15 15:17:30 +0700

    Update and rename twinny-property.js to new-empty.js

---
+++ new-empty.js
@@ -0,0 +1,20 @@
+/* ================================================================================================
+
+    Peringatan !!
+
+===================================================================================================
+
+    Mengubah / Menghapus file javascript (js) ini dapat mengakibatkan :
+
+    1. Kerusakan / error pada template
+    2. Menghilangkan fungsi update / perbaikan otomatis pada template
+    3. Tidak akan mendapatkan support/bantuan dari kreator template
+
+    Terima kasih sudah menggunakan Template Dari Twinny Blogger. :)
+
+    Salam,
+    Twinny Blogger - Twinny Property
+
+    Terakhir Diperbarui : 26/02/2022 @ 18:13
+
+================================================================================================ */
